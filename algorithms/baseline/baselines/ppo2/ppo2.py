a41df47e259aee8ece398b874173a0be5c19288b
hugopiq <piquardhugo@yahoo.fr>
2022-01-24 09:07:55 +0100

    ok

--- algorithms/baseline/baselines/ppo2/ppo2.py
+++ algorithms/baseline/baselines/ppo2/ppo2.py
@@ -147,7 +147,7 @@ def learn(*, network, env, total_timesteps, eval_env = None, seed=None, nsteps=2
 
         # Get minibatch
         obs, returns, masks, actions, values, neglogpacs, states, epinfos = runner.run() #pylint: disable=E0632
-
+        print("Get mini batch!")
         if eval_env is not None:
             eval_obs, eval_returns, eval_masks, eval_actions, eval_values, eval_neglogpacs, eval_states, eval_epinfos = eval_runner.run() #pylint: disable=E0632
 
