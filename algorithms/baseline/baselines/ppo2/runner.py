761128b3f6359f0c889f052db6f0d107e3200883
hugopiq <piquardhugo@yahoo.fr>
2022-01-25 17:40:59 +0100

    ok

--- algorithms/baseline/baselines/ppo2/runner.py
+++ algorithms/baseline/baselines/ppo2/runner.py
@@ -62,7 +62,6 @@ class Runner(AbstractEnvRunner):
             delta = mb_rewards[t] + self.gamma * nextvalues * nextnonterminal - mb_values[t]
             mb_advs[t] = lastgaelam = delta + self.gamma * self.lam * nextnonterminal * lastgaelam
         mb_returns = mb_advs + mb_values
-        print(mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs)
         return (*map(sf01, (mb_obs, mb_returns, mb_dones, mb_actions, mb_values, mb_neglogpacs)),
             mb_states, epinfos)
 # obs, returns, masks, actions, values, neglogpacs, states = runner.run()
