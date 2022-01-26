a66fd94e4cf64f43d50d90e0ad556eba2b4a39a5
hugopiq <piquardhugo@yahoo.fr>
2022-01-26 08:04:46 +0100

    test tf

--- environments/examples/ROBOT/gg_random.py
+++ environments/examples/ROBOT/gg_random.py
@@ -8,6 +8,7 @@ import time
 env = gym.make('MYROBOT-v0')
 
 while True:
+    time.sleep(2000)
     # take a random action
     observation, reward, done, info = env.step([0, 1, ])
     time.sleep(5000)
