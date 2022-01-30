f5cef6359b6e141852607ce3024776ced0ac83b0
hugopiq <piquardhugo@yahoo.fr>
2022-01-30 13:42:53 +0100

    ok

--- environments/gym_gazebo2/utils/ut_launch.py
+++ environments/gym_gazebo2/utils/ut_launch.py
@@ -302,7 +302,7 @@ def generateLaunchDescriptionCARS(gzclient, realSpeed, multiInstance, port, urdf
 
     if realSpeed:
         worldPath = os.path.join(os.path.dirname(gym_gazebo2.__file__), 'worlds',
-                                 'empty.world')
+                                 'track.world')
     else:
         worldPath = os.path.join(os.path.dirname(gym_gazebo2.__file__), 'worlds',
                                  'empty_speed_up.world')
